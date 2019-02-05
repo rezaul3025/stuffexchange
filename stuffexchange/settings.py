ce793e07dbaa3b9ee99615adb3c712628e6582c1
Rezaul Karim <rkarim@primervoto.de>
2019-02-05 09:28:02 +0100

    init

--- stuffexchange/settings.py
+++ stuffexchange/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-_82clu12jli8!y8dueb()9oo75ip_wwxxr4_*nh2$d%=ibt5t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'rezaulkm105.pythonanywhere.com']
 
--- stuffexchange/urls.py
+++ stuffexchange/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('registration/', views.registration, name='registration')
 ]
